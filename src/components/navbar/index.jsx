44ac14e7bee32d894e8e167523d86f6d15b391b8
BSbenjaminsoto <benjaminsoto661@hotmail.com>
2023-12-01 18:06:42 -0300

    Disenando dashboard y creacion de navbar

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Navbar.css"
+import { Link } from "react-router-dom";
+
+
+function Navbar() {
+    return(
+    <>
+    <div className="container-navbar">
+        <h2 className="h2-navbar" >Killcoronavirus</h2>
+        <div>
+        <Link className="navbar-link" to={"/"} ><b>Inicio</b></Link>
+        <Link className="navbar-link" to={"/"} >Administrar</Link>
+        <Link className="navbar-link" to={"/"} >Pacientes</Link>
+        <Link className="navbar-link" to={"/"} >Fichas</Link>
+        <Link className="navbar-link" to={"/"}>Salir</Link>
+        </div>
+    </div>
+    </>
+    )
+}
+
+export default Navbar;
