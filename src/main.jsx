e74b347a8285c171f03027bbc939fe29bcebedd7
BSbenjaminsoto <benjaminsoto661@hotmail.com>
2023-12-01 18:49:05 -0300

    Instalando RobotoFonts

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,12 @@ import {
 import App from './App.jsx'
 import Dashboard from './components/dashboard'
 import './index.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 
 const router = createBrowserRouter([
   {
